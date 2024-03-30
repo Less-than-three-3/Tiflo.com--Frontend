e677a3c8819c887508e0f9cdeda6889ea784ad36
IvanStukalov <ivanstukalov2003@gmail.com>
2024-03-30 23:47:17 +0300

    wavesurfer experiments & readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Frontend
+# Tiflo.com Frontend
+
+### Get Started (dev)
+
+Для всего этого шаманства нужен NodeJS (у меня версия 18.15.0, можно и старше по идее)
+
+Если его нет, то ставим. [вот ссылка на инструкцию](https://nodejs.org/en/download/package-manager)
+
+Проверяем NodeJS командой `node -v`
+
+1. Клонируем репу `git clone https://github.com/Less-than-three-3/Tiflo.com--Frontend.git`
+2. В корне проекта устанавливаем зависимости `npm install`
+3. Для разработки нужно две команды (выполняем также в корне проекта):
+    - `npm run start` - запускаем сервер фронта
+    - `npm run tw-w` - запускаем watcher тайлвинда. Нужен для автоматической генерации стилей из css-файлов и из верстки. 
--- src/components/sections/AudioEditor/AudioEditor.jsx
+++ src/components/sections/AudioEditor/AudioEditor.jsx
@@ -1,5 +1,7 @@
-import {useCallback, useRef} from "react";
+import {useCallback, useEffect, useMemo, useRef} from "react";
 import {useWavesurfer} from '@wavesurfer/react'
+import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js'
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'
 
 export const AudioEditor = () => {
   const contaienrRef = useRef(null);
@@ -11,6 +13,7 @@ export const AudioEditor = () => {
     url: '/src/assets/audio.mp3',
     cursorColor: "#fff",
     cursorWidth: 2,
+    plugins: useMemo(() => [Timeline.create()], []),
   });
 
   wavesurfer?.on('interaction', () => {
@@ -20,6 +23,36 @@ export const AudioEditor = () => {
   const onPlayPause = useCallback(() => {
     wavesurfer && wavesurfer.playPause()
   }, [wavesurfer])
+
+  const wsRegions = wavesurfer?.registerPlugin(RegionsPlugin.create())
+
+  wavesurfer?.on('decode', () => {
+    // Regions
+    wsRegions.addRegion({
+      start: 9,
+      end: 30,
+      content: 'Tap & Resize & Drag',
+      color: "rgba(117,6,255,0.49)",
+      drag: true,
+      resize: true,
+    })
+
+    wsRegions.addRegion({
+      start: 46.5,
+      content: 'Marker',
+      color: "rgb(6,160,255)",
+      drag: true,
+      resize: true,
+    })
+  })
+
+  let activeRegion = null
+  wsRegions?.on('region-clicked', (region, e) => {
+    e.stopPropagation() // prevent triggering a click on the waveform
+    activeRegion = region
+    region.play()
+    region.setOptions({ color: "rgba(80,89,255,0.49)" })
+  })
 
   return (
     <>

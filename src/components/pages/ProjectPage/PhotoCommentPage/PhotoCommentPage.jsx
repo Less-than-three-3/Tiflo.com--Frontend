3496577621ac8274cb72080800a927966e7780c3
IvanStukalov <ivanstukalov2003@gmail.com>
2024-05-04 21:29:38 +0300

    fix navigation

--- src/components/pages/ProjectPage/PhotoCommentPage/PhotoCommentPage.jsx
+++ src/components/pages/ProjectPage/PhotoCommentPage/PhotoCommentPage.jsx
@@ -3,17 +3,20 @@ import {TextEditor} from "../../../sections/TextEditor/TextEditor.jsx";
 import {useProject} from "../../../../hooks/useProject.js";
 import {useParams} from "react-router-dom";
 import {api} from "../../../../api/api.js";
+import {useEffect} from "react";
 
 export const PhotoCommentPage = () => {
   const {setProject} = useProject();
 
   const params = useParams();
-  (async () => {
-    const getProjectRes = await api.getProjectById(params.projectId);
-    if (getProjectRes.status === 200) {
-      setProject(getProjectRes.data);
-    }
-  })()
+  useEffect(() => {
+    (async () => {
+      const getProjectRes = await api.getProjectById(params.projectId);
+      if (getProjectRes.status === 200) {
+        setProject(getProjectRes.data);
+      }
+    })()
+  })
 
   return (
     <>

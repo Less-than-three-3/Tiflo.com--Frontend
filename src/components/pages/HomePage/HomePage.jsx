9a767f902512c713b65689ebc3208ee8302db651
IvanStukalov <ivanstukalov2003@gmail.com>
2024-05-08 20:03:47 +0300

    fix

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,4 +1,13 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/project/photo");
+  }, [])
+
   return (
     <>
       <h1>Home</h1>
